f59060bd99704ac9a7240b8576aea7eb4708bdfa
joker <149232318+NFraw@users.noreply.github.com>
2024-07-15 16:03:15 +0800

    Update 001.c

--- 船说C语言学习笔记/001.c
+++ 船说C语言学习笔记/001.c
@@ -1,10 +1,22 @@
+/*
+* printf函数原型：
+* 头文件：stdio.h
+* 原型：int printf(const char* format, ...);
+* format：格式控制字符串
+* ...：可变参数列表
+* 返回值：输出字符的数量
+*/
+/*
+* 题目：请使用printf函数，求解一个数字n的十进制表示的数字位数
+*/
+
+
 #include<stdio.h>
 
 int main() {
-
-
-
-
-
+	int n, digit;
+	scanf_s("%d", &n);
+	digit = printf("%d", n);
+	printf(" has %d digit(s)\n", digit);
 	return 0;
 }
