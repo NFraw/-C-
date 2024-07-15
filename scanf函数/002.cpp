9dc43849df53f7aaa86d8acfb643dbf5954ab699
joker <149232318+NFraw@users.noreply.github.com>
2024-07-15 20:19:14 +0800

    Create 002.cpp

---
+++ scanf函数/002.cpp
@@ -0,0 +1,25 @@
+/*
+* scanf函数原型：
+* 头文件：stdio.h
+* 原型：int scanf(const char *format, ...);
+* format：格式控制字符串
+* ...：可变参数列表
+* 返回值：成功读入的 参数 个数
+*/
+/*
+* scanf特点：
+* 1.格式化读入
+* 比如输入是IP地址101.32.03.10之类的
+* 那么 scanf(%d.%d.%d.%d);则可以直接忽略掉里面的"."
+* 2.
+* 
+* 
+*/
+
+
+#include<stdio.h>
+
+int main() {
+
+	return 0;
+}
