35877752efea4ad9c3406d9b56bb441062001bd2
NFraw <1470161695@qq.com>
2024-07-30 12:01:33 +0800

    Create 3.text1.c

---
+++ 第一章代码/3.text1.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+        > File Name: 3.text1.c
+        > Author:ZhangNan 
+        > Mail:1470161695@qq.com 
+        > Created Time: Sun 21 Jul 2024 09:03:08 AM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main(){
+    int n, digit;
+    scanf("%d", &n);
+    digit = printf("%d", n);
+    printf(" has %d digit(s)\n", digit);
+    return 0;
+}
